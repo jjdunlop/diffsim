5bfa4f4a8fc1b7a8e0a5723067201aae7ce7e5e9
jjdunlop <66872523+jjdunlop@users.noreply.github.com>
2024-03-06 13:27:05 +1100

    Add files via upload

---
+++ installDependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update
+
+# Install all required packages
+sudo apt-get install -y --no-install-recommends \
+    gcc-4.8 \
+    gcc-5 \
+    libblas-dev \
+    liblapack-dev \
+    libopenblas-dev \
+    gfortran \
+    scons \
+    libpng-dev \
+    git \
+    libgl1-mesa-glx \
+    libhdf5-dev \
+    libglib2.0-0 \
+    openmpi-bin \
+    wget \
+    apt-utils \
+    curl \
+    vim \
+    build-essential \
+    cmake \
+    libboost-dev \
+    libboost-all-dev \
+    mesa-common-dev \
+    libglu1-mesa-dev \
+    unzip \
+    gcc-4.8 gcc-5 \
+    g++-4.8 g++-5 \
+    libblas-dev liblapack-dev \
+    gfortran-4.8
+
+# Make the arcsim configure script executable
+chmod +x /arcsim/dependencies/taucs/configure
